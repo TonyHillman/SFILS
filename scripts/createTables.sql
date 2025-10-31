a5bc81f9abeb4234d2ea3ba96839e5e2c5d56e10
TonyHillman <anthonylhillman@gmail.com>
2025-10-30 18:06:18 -0800

    fixes for createTables
    successfully imported data with loadData

---
+++ scripts/createTables.sql
@@ -0,0 +1,42 @@
+USE library_db;
+
+
+CREATE TABLE PATRON_TYPE (
+    patron_type_code TINYINT PRIMARY KEY,
+    patron_type_definition VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE HOME_LIBRARY (
+    library_code CHAR(5) PRIMARY KEY,
+    library_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE NOTICE_PREFERENCE (
+    notice_preference_code CHAR(3) PRIMARY KEY,
+    notice_definition CHAR(10) NOT NULL
+);
+
+CREATE TABLE PATRON (
+    patron_id INT AUTO_INCREMENT PRIMARY KEY,
+    patron_type_code TINYINT,
+    age_range VARCHAR(25),
+    home_library_code CHAR(5),
+    notice_preference_code CHAR(5),
+    email_provided BOOLEAN,
+    year_registered YEAR,
+    within_sf_county BOOLEAN,
+    FOREIGN KEY (patron_type_code) REFERENCES PATRON_TYPE(patron_type_code),
+    FOREIGN KEY (home_library_code) REFERENCES HOME_LIBRARY(library_code),
+    FOREIGN KEY (notice_preference_code) REFERENCES NOTICE_PREFERENCE(notice_preference_code)
+);
+
+
+CREATE TABLE CIRCULATION_ACTIVITY (
+    circulation_id INT AUTO_INCREMENT PRIMARY KEY,
+    total_checkouts INT,
+    total_renewals INT,
+    active_month CHAR(9),
+    active_year YEAR,
+    patron_id INT,
+    FOREIGN KEY (patron_id) REFERENCES PATRON(patron_id)
+);
---
+++ scripts/loadData.sql
@@ -0,0 +1,33 @@
+USE library_db;
+
+SHOW VARIABLES LIKE 'secure_file_priv';
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/SFPL_DataSF_library-usage_Jan_2023_CLEAN.csv'
+INTO TABLE temp_data
+FIELDS TERMINATED BY ',' 
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(
+  patron_type_code,
+  patron_type_definition,
+  total_checkouts,
+  total_renewals,
+  age_range,
+  home_library_code,
+  home_library_name,
+  circulation_active_month,
+  @circulation_active_year,
+  notice_preference_code,
+  notice_definition,
+  @email_provided,
+  @within_sf_county,
+  year_registered
+)
+SET
+  circulation_active_year = NULLIF(@circulation_active_year, ''),
+  email_provided = IF(LOWER(@email_provided) IN ('true', '1'), 1, 0),
+  within_sf_county = IF(LOWER(@within_sf_county) IN ('true', '1'), 1, 0);
+
+SELECT COUNT(*) FROM temp_data;
+
